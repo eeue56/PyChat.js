62c1e2ff3f49e2208ab776c1d49e63660ba04604
eeue56 <eeue56@bangor.ac.uk>
2013-03-13 23:46:48 +0000

    No need to provide send time, the DB can do that for you

--- database/updates.py
+++ database/updates.py
@@ -8,10 +8,8 @@ class Conn(object):
         c = self.connection.cursor()
 	c.execute("INSERT INTO Users VALUES(?)", (username,))
 
-    def insert_new_message(self, username, room_name, send_time):
-
+    def insert_new_message(self, username, room_name):
 	c = self.connnection.cursor()
-        if not send_time:
-            c.execute("INSERT INTO Messages VALUES(username = ?, room_name = ?)",(username, room_name))
-        else:
-            c.execute("INSERT INTO Messages VALUES(username = ? room_name = ? send_time = ?)",(username, room_name, send_time))
+       	c.execute("INSERT INTO Messages VALUES(username = ?, "
+		  "room_name = ?)",
+		  (username, room_name))
