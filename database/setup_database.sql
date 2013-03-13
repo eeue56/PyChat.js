5d1e5edb629109419ae10f2b71b60f790452af0b
eeue56 <eeue56@bangor.ac.uk>
2013-03-13 23:44:51 +0000

    rename fields

--- database/setup_database.sql
+++ database/setup_database.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS Sockets;
+USE Sockets;
+
 CREATE TABLE IF NOT EXISTS Users
 (
 	username varchar(20),
@@ -6,34 +9,15 @@ CREATE TABLE IF NOT EXISTS Users
 
 CREATE TABLE IF NOT EXISTS Messages
 (
-	messageID INTEGER PRIMARY KEY AUTOINCREMENT,
-	sendTime timestamp DEFAULT CURRENT_TIMESTAMP,
+	message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	time_sent timestamp DEFAULT CURRENT_TIMESTAMP,
 	username varchar(20),
-	roomName varchar(20),
-	FOREIGN KEY(roomName) REFERENCES Rooms(roomName),
+	room_name varchar(20),
+	FOREIGN KEY(room_name) REFERENCES Rooms(room_name),
 	FOREIGN KEY(username) REFERENCES Users(username)
 );
 
 CREATE TABLE IF NOT EXISTS Rooms
 (
-	roomName varchar(20)
+	room_name varchar(20)
 );
-
-/*
-CREATE TABLE Meetings
-(
-	startTime datetime,
-	endTime datetime,
-	meetingName varchar(20),
-	PRIMARY KEY(meetingName)
-);
-
-CREATE TABLE Attending
-(
-	meetingID varchar(20),
-	username varchar(20),
-	PRIMARY KEY(meetingID, username),
-	FOREIGN KEY(meetingID) REFERENCES Meetings(meetingID),
-	FOREIGN KEY(username) REFERENCES Users(username)
-);
-*/
