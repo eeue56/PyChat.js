173ab1ee00c2fd9f2d4c107244eeac028b7bfaa4
eeue56 <eeue56@bangor.ac.uk>
2013-03-13 16:17:20 +0000

    adding stuff

--- server/server.py
+++ server/server.py
@@ -3,18 +3,33 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.web
 
+from random import choice
+
 
 connections = []
 
+usernames = ['Shauna', 'Tomuel', 'Darkok']
+
+class User(object):
+	def __init__(self, name):
+		self.name = name
+
+	def __str__(self):
+		return str(self.name)
+
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
+    	self.id = User(choice(usernames) or 'Durp')
+    	usernames.remove(self.id.name)
     	connections.append(self)
         print 'new connection'
-        self.write_message("There are currently {con} connections".format(con=len(connections)))
+        self.write_message("There are currently {con} connections\n\n".format(con=len(connections)))
         
       
     def on_message(self, message):
-        print 'message received %s' % message
+    	print message
+        for con in connections:
+        	con.write_message('{id} says: {mes}\n'.format(id=self.id.name, mes=message))
  
     def on_close(self):
     	connections.remove(self)
