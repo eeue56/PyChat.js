7a541382738885bfeb01d42a29d92d3fbc79df9d
lilrooness <jfrangoudes@gmail.com>
2013-03-14 10:59:17 +0000

    Able to add rooms users and messages using functions from updates.py

--- database/updates.py
+++ database/updates.py
@@ -6,10 +6,12 @@ class Conn(object):
 
     def insert_new_user(self, username):
         c = self.connection.cursor()
-	c.execute("INSERT INTO Users VALUES(?)", (username,))
+        c.execute("INSERT INTO Users VALUES(?)", (username,))
 
     def insert_new_message(self, username, room_name):
-	c = self.connnection.cursor()
-       	c.execute("INSERT INTO Messages VALUES(username = ?, "
-		  "room_name = ?)",
-		  (username, room_name))
+	   c = self.connnection.cursor()
+       c.execute("INSERT INTO Messages VALUES(username = ?, room_name = ?)",(username, room_name))
+
+    def insert_new_room(self, room_name):
+        c = self.connection.cursor()
+        c.execute("INSERT INTO Rooms VALUES(room_name = ?)", (room_name))
