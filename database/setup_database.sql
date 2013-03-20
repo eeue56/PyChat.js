517453dbb88d12289f7fcf4f5d06c0fddfe9489f
lilrooness <jfrangoudes@gmail.com>
2013-03-20 12:38:55 +0000

    Added documentation folder and created a flow chart describing join room process

--- database/setup_database.sql
+++ database/setup_database.sql
@@ -3,21 +3,21 @@ USE Sockets;
 
 CREATE TABLE IF NOT EXISTS Users
 (
-	username varchar(20),
-	PRIMARY KEY(username)
+    username varchar(20),
+    PRIMARY KEY(username)
 );
 
 CREATE TABLE IF NOT EXISTS Messages
 (
-	message_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	time_sent timestamp DEFAULT CURRENT_TIMESTAMP,
-	username varchar(20),
-	room_name varchar(20),
-	FOREIGN KEY(room_name) REFERENCES Rooms(room_name),
-	FOREIGN KEY(username) REFERENCES Users(username)
+    message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time_sent timestamp DEFAULT CURRENT_TIMESTAMP,
+    username varchar(20),
+    room_name varchar(20),
+    FOREIGN KEY(room_name) REFERENCES Rooms(room_name),
+    FOREIGN KEY(username) REFERENCES Users(username)
 );
 
 CREATE TABLE IF NOT EXISTS Rooms
 (
-	room_name varchar(20)
+    room_name varchar(20)
 );
