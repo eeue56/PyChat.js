b8c8427e747675498e0758f9908b93ff8826a6aa
lilrooness <jfrangoudes@gmail.com>
2013-03-20 10:11:50 +0000

    loggin request not needed in template - template changed

--- database/updates.py
+++ database/updates.py
@@ -7,11 +7,14 @@ class Conn(object):
     def insert_new_user(self, username):
         c = self.connection.cursor()
         c.execute("INSERT INTO Users VALUES(?)", (username,))
+        connection.commit()
 
     def insert_new_message(self, username, room_name):
-	   c = self.connnection.cursor()
+       c = self.connnection.cursor()
        c.execute("INSERT INTO Messages VALUES(username = ?, room_name = ?)",(username, room_name))
+       connection.commit()
 
     def insert_new_room(self, room_name):
         c = self.connection.cursor()
-        c.execute("INSERT INTO Rooms VALUES(room_name = ?)", (room_name))
+        c.execute("INSERT INTO Rooms VALUES(room_name = ?)", (room_name,))
+        connection.commit()
