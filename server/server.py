c28e21582058e576eb942649ee9945da26a5fe48
eeue56 <eeue56@bangor.ac.uk>
2013-03-21 12:59:19 +0000

    stripped out functionality

--- server/server.py
+++ server/server.py
@@ -11,9 +11,9 @@ from room import *
 import logging
 
 from json import loads, dumps
+from connections import ChatConnection
+from parser import Parser
 
-
-rooms = [Room('Darkness')]
 
 usernames = ['Shauna', 'Tomuel', 'Darkok']
 
@@ -29,63 +29,6 @@ class User(object):
         usernames.append(self.name)
 
 
-class ChatConnection(object):
-    def __init__(self, username, handler):
-        self.id = username
-        self._rooms = []
-        self.handler = handler
-        self.parse_join('join Darkness')
-
-    def write_message(self, message):
-        logging.debug("Sending message {mes} to {usr}".format(mes=message, usr=self.id))
-        self.handler.write_message(message)
-
-    def join_room(self, room_name):        
-        logging.debug('Joining room {ro}'.format(ro=room_name))
-
-        for room in rooms:
-            if room.name == room_name:
-                room.add_user(self)
-                return room
-        raise RoomNotFoundException('No such room as {name}!'.format(name=room_name))
-
-    def _send_to_all_rooms(self, message):
-        for room in self._rooms:
-            room.send_message(message)
-
-    def parse_nick(self, message):
-        logging.info('Parsing nick...')
-        previous_name = self.id.name
-        self.id.release_name()
-        self.id = User(message[message.index('nick') + 4:].strip())
-        
-        logging.debug('Nick set as {name}'.format(name=self.id))
-        self._send_to_all_rooms('User {n} is now known as {nick}\n'.format(n=previous_name, nick=self.id.name))
-      
-    def parse_join(self, message):
-        logging.info('Parsing join...')
-        room_name = message[message.index('join') + 4:].strip()
-
-        logging.debug('Room name set as {name}'.format(name=room_name))
-
-        try:
-            room = self.join_room(room_name)
-            self._rooms.append(room)
-            room.welcome(self)
-        except RoomNotFoundException:
-            logging.debug('Room not found!')
-            self.write_message('No such room!\n')
-
-    def close(self):
-        self.id.release_name()
-        
-        for room in self._rooms:
-            room.remove_user(self)
-
-    def pong(self):
-        logging.info('Sending pong..')
-        self.handler.write_message('{"service":2}')
-
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print 'new connection'
@@ -98,33 +41,19 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         else:
             id_ = User('Guest {i}'.format(i=len(connections)))
 
-        self.conn = ChatConnection(id_, self)
+        self.parser = Parser(ChatConnection(id_, self))
 
         logging.info('User with name {name} joined!'.format(name=id_))
 
     def on_message(self, message):
         logging.info('Message {mes} recieved from user {id}'.format(mes=message, 
-            id=self.conn.id))
+            id=self.parser.connection.id))
 
-        if message.startswith('\\'):
-            if 'nick' in message[:7]:
-               self.conn.parse_nick(message)
-            elif 'join' in message[:7]:
-                self.conn.parse_join(message)
-            return 
-
-        if 'command' in message:
-            logging.info('command recieved ' + message)
-            m = loads(message)
-
-            if m['command'] == 1:
-                self.conn.pong()
-            return
-            
-        self.conn._send_to_all_rooms('{id} says: {mes}\n'.format(id=self.conn.id, mes=message))
+        self.parser.parse_message(message)
+    
  
     def on_close(self):
-        logging.info('User {id} disconnected!'.format(id=self.conn))
+        logging.info('User {id} disconnected!'.format(id=self.parser.connection.id))
         self.conn.close()
 
         self.write_message('Connection closed')
