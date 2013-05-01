164978d39bf8f4f77305ae239392157fcacc6250
subsidel <subsidel@gmail.com>
2013-05-01 15:47:12 +0200

    Create get_slides.php
    initial version, for software hut presentation only

---
+++ pychatjs/server/PHP/get_slides.php
@@ -0,0 +1,14 @@
+<?php
+
+$for_presentation_only = {"hutSlides/slide1.png",
+  		"hutSlides/slide2.png",
+			"hutSlides/slide3.png",
+			"hutSlides/slide4.png",
+			"hutSlides/slide5.png",
+			"hutSlides/slide6.png"};
+
+$Json_slides = json_encode($for_presentation_only);
+
+echo($Json_slides);
+
+?>
