f182d218676adf97664dfa2af4bbf01a2922b3ec
eeue56 <eeue56@bangor.ac.uk>
2013-05-07 15:02:38 +0000

    fixing name validation;

--- client/js/main.js
+++ client/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
     var init = function(userName, avatarUrl) {
         // create an avatar
+
         var avatar = new Image();
 
 	if (typeof avatarUrl === "undefined" || avatarUrl == null){
@@ -148,11 +149,18 @@ $(document).ready(function () {
     $(".enter").click(function () {
         var userName = $("#name").val();
         var avatarUrl = $("#avatar-url").val();
-        if(avatarUrl.length == 0) {
-            avatarUrl = "img/blank.png"
+
+	if (typeof avatarUrl === "undefined" || avatarUrl == null || avatarUrl == ""){
+            avatarUrl = "img/blank.png";
+        }
+	        
+	if (typeof userName === "undefined" || userName == null || userName == ""){
+            alert("Username not valid!");
+	    return;
         }
         init(userName, avatarUrl);
         $(".name").fadeOut();
+        
     });
 
     pyjs.find(".pyjs-conversation-send").click(function() {
@@ -170,15 +178,6 @@ $(document).ready(function () {
 
         cs.send(msgReq);
     };
-
-
-    $(".name").show();
-
-    $(".enter").click(function () {
-        var userName = $("#name").val();
-        init(userName);
-        $(".name").fadeOut();
-    });
 
     $(".pyjs-conversation-message").keypress(function (e) {
         if (e.which == 13){
