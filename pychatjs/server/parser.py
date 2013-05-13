a469090b80968749a80432f0023d08ae183ad6ff
lilrooness <jfrangoudes@gmail.com>
2013-05-13 19:04:18 +0100

    Parser.py now uses a dict to call functions

--- pychatjs/server/parser.py
+++ pychatjs/server/parser.py
@@ -10,6 +10,17 @@ class Parser(object):
         TODO: Change to use dict to allow easy extension"""
     def __init__(self, connection):
         self.connection = connection
+        self.functions = {'ping' : ping, 
+            'roomlist' : roomlist, 
+            'join' : join,
+            'userlist' : userlist,
+            'send' : send,
+            'get_user_dump' : get_user_dump,
+            'get_users_dump' : get_users_dump,
+            'send_dump' : send_dump,
+            'next_slide' : next_slide,
+            'previous_slide' : previous_slide,
+            'request_name' : request_name}
 
 
     def parse_message(self, message):  
@@ -32,74 +43,68 @@ class Parser(object):
             return
 
         request_name = requests[service]
-
-        # process requests without data
-        if request_name == 'ping':
-            conn.write_message(create_pong())
-            return
-        elif request_name == 'roomlist':
-            conn.write_message(create_roomlist(conn.possible_rooms))
-            return
-
-
         data = get_data(message)
 
         logging.debug(data)
 
-        # process requests with data
+        self.functions[request_name](data)
+
+    def ping(data):
+    conn.write_message(create_pong())
+
+    def roomlist(data):
+        conn.write_message(create_roomlist(conn.possible_rooms))
         
-        if request_name == 'join':
-            logging.debug("Name is " + conn.id.name)
-            logging.debug("Name should be " + data['username'])
-            if conn.id.name != data['username']:   
-                try:             
-                    conn.id.change_name(data['username'])
-                except UsernameInUseException:
-                    conn.write_message(create_error(2, 'Username was already in use!'))
-            conn.join_room(data['room'])
-            conn._send_to_all_rooms(create_connect(conn.id.name))
-
-        elif request_name == 'userlist':
-            room = conn.get_room(data['room'])
-
-            if room is None:
-                conn.write_message('No such room as {room}'.format(room=data['room']))
-            else:
-                conn.write_message(create_userlist(room.user_names))
-
-        elif request_name == 'send':
-            room = conn.get_room(data['room'])
-
-            if room is not None:
-                room.send_message(create_message(data['username'], data['message']))
-
-        elif request_name == 'get_user_dump':
-            username = data['username']
-            for room in conn.possible_rooms:
-                if room.get_user(username) is not None:
-                    conn.write_message(create_user_dump(room.get_user(username)))
-                    break
-            else:
-                conn.write_message(create_error(3, 'User not found in any active rooms'))
-
-        elif request_name == 'get_users_dump':
-            room = conn.get_room(data['room'])
-
-            if room is not None: 
-                conn.write_message(create_users_dump(room.get_users_connected))
-
-        elif request_name == 'send_dump':
-            for prop, value in data.iteritems():
-                setattr(conn.id, prop, value)
-
-        elif request_name == 'next_slide':
-            room = conn.get_room(data['room'])
-            room.send_message(create_next_slide())
-       
-        elif request_name == 'previous_slide':
-            room = conn.get_room(data['room'])
-            room.send_message(create_previous_slide())
-       
-        elif request_name == 'jump_to_slide':
-            room = conn.get_room(data['room'])
-            room.send_message(create_jump_to(data['slideNumber']))
+    def join(data):
+        logging.debug("Name is " + conn.id.name)
+        logging.debug("Name should be " + data['username'])
+        if conn.id.name != data['username']:   
+            try:             
+                conn.id.change_name(data['username'])
+            except UsernameInUseException:
+                conn.write_message(create_error(2, 'Username was already in use!'))
+        conn.join_room(data['room'])
+        conn._send_to_all_rooms(create_connect(conn.id.name))
+    
+    def userlist(data):
+        room = conn.get_room(data['room'])
+        if room is None:
+            conn.write_message('No such room as {room}'.format(room=data['room']))
+        else:
+            conn.write_message(create_userlist(room.user_names))
+    
+    def send(data):
+        room = conn.get_room(data['room'])
+        if room is not None:
+            room.send_message(create_message(data['username'], data['message']))
+    
+    def get_user_dump(data):
+        username = data['username']
+        for room in conn.possible_rooms:
+            if room.get_user(username) is not None:
+                conn.write_message(create_user_dump(room.get_user(username)))
+                break
+        else:
+            conn.write_message(create_error(3, 'User not found in any active rooms'))
+    
+    def get_users_dump(data):
+        room = conn.get_room(data['room'])
+        if room is not None: 
+            conn.write_message(create_users_dump(room.get_users_connected))
+    
+    def send_dump(data):
+        for prop, value in data.iteritems():
+            setattr(conn.id, prop, value)
+    
+    def next_slide(data):
+        room = conn.get_room(data['room'])
+        room.send_message(create_next_slide())
+    
+    def previous_slide(data):
+        room = conn.get_room(data['room'])
+        room.send_message(create_previous_slide())
+    
+    def request_name(data):
+        room = conn.get_room(data['room'])
+        room.send_message(create_jump_to(data['slideNumber']))
+    
