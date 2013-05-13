53912f6ab935ed6b5be4f9db8c3cf7d451dfaefb
lilrooness <jfrangoudes@gmail.com>
2013-05-13 19:25:47 +0100

    fixed self. functions

--- pychatjs/server/parser.py
+++ pychatjs/server/parser.py
@@ -10,17 +10,17 @@ class Parser(object):
         TODO: Change to use dict to allow easy extension"""
     def __init__(self, connection):
         self.connection = connection
-        self.functions = {'ping' : ping, 
-            'roomlist' : roomlist, 
-            'join' : join,
-            'userlist' : userlist,
-            'send' : send,
-            'get_user_dump' : get_user_dump,
-            'get_users_dump' : get_users_dump,
-            'send_dump' : send_dump,
-            'next_slide' : next_slide,
-            'previous_slide' : previous_slide,
-            'request_name' : request_name}
+        self.functions = {'ping' : self.ping, 
+            'roomlist' : self.roomlist, 
+            'join' : self.join,
+            'userlist' : self.userlist,
+            'send' : self.send,
+            'get_user_dump' : self.get_user_dump,
+            'get_users_dump' : self.get_users_dump,
+            'send_dump' : self.send_dump,
+            'next_slide' : self.next_slide,
+            'previous_slide' : self.previous_slide,
+            'request_name' : self.request_name}
 
 
     def parse_message(self, message):  
@@ -49,13 +49,13 @@ class Parser(object):
 
         self.functions[request_name](data)
 
-    def ping(data):
+    def ping(self, data):
         conn.write_message(create_pong())
 
-    def roomlist(data):
+    def roomlist(self, data):
         conn.write_message(create_roomlist(conn.possible_rooms))
         
-    def join(data):
+    def join(self, data):
         logging.debug("Name is " + conn.id.name)
         logging.debug("Name should be " + data['username'])
         if conn.id.name != data['username']:   
@@ -66,19 +66,19 @@ class Parser(object):
         conn.join_room(data['room'])
         conn._send_to_all_rooms(create_connect(conn.id.name))
     
-    def userlist(data):
+    def userlist(self, data):
         room = conn.get_room(data['room'])
         if room is None:
             conn.write_message('No such room as {room}'.format(room=data['room']))
         else:
             conn.write_message(create_userlist(room.user_names))
     
-    def send(data):
+    def send(self, data):
         room = conn.get_room(data['room'])
         if room is not None:
             room.send_message(create_message(data['username'], data['message']))
     
-    def get_user_dump(data):
+    def get_user_dump(self, data):
         username = data['username']
         for room in conn.possible_rooms:
             if room.get_user(username) is not None:
@@ -87,24 +87,24 @@ class Parser(object):
         else:
             conn.write_message(create_error(3, 'User not found in any active rooms'))
     
-    def get_users_dump(data):
+    def get_users_dump(self, data):
         room = conn.get_room(data['room'])
         if room is not None: 
             conn.write_message(create_users_dump(room.get_users_connected))
     
-    def send_dump(data):
+    def send_dump(self, data):
         for prop, value in data.iteritems():
             setattr(conn.id, prop, value)
     
-    def next_slide(data):
+    def next_slide(self, data):
         room = conn.get_room(data['room'])
         room.send_message(create_next_slide())
     
-    def previous_slide(data):
+    def previous_slide(self, data):
         room = conn.get_room(data['room'])
         room.send_message(create_previous_slide())
     
-    def request_name(data):
+    def request_name(self, data):
         room = conn.get_room(data['room'])
         room.send_message(create_jump_to(data['slideNumber']))
     
